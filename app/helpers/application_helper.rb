7c85156308d08bd8edd6291a187cffd9b74c39e7
Torsten Ruger <torsten@villataika.fi>
2018-01-18 10:05:01 +0200

    add vue from cdn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def vue_script_tag
+    prod = Rails.env.production? ? ".min" : ""
+    src = "https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue#{prod}.js"
+    "<script src='#{src}'></script>".html_safe
+  end
 end
