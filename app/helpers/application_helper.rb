b2f620d2ea26b0b7730741399a389d7c073eac90
Torsten Ruger <torsten@villataika.fi>
2018-01-30 17:25:22 +0200

    add assets for off line work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def vue_script_tag
     prod = Rails.env.production? ? ".min" : ""
-    src = "https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue#{prod}.js"
+    src = asset_path "vue#{prod}.js"
     "<script src='#{src}'></script>".html_safe
   end
 end
