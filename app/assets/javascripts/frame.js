3cbf419e95490300fa3f3bb9d1bd77ee2c51bff4
Torsten Ruger <torsten@villataika.fi>
2018-02-09 11:07:09 +0800

    refactor into posts into own file

--- app/assets/javascripts/frame.js
+++ app/assets/javascripts/frame.js
@@ -1,39 +1,12 @@
 //= require xeogl
+//= require post_beam
+
 
 xeogl.scene = new xeogl.Scene({ canvas: "xeocanvas" });
 xeogl.scene.material.diffuse = [ 0.6, 0.6, 0.7 ] ;
 xeogl.scene.camera.view.eye = [-400, 200, -1000];
 new xeogl.CameraControl();
 
-eco = {}
-eco.PostBeam = xeogl.Model.extend({
-  type: "eco.PostBeam",
-  size: 6,
-  height: 200,
-  width: 300,
-  braces: true,
-  _init: function( ){
-    xeogl.Model.prototype._init.apply(this , [{}]); // super()
-    var post = new xeogl.BoxGeometry( { xSize: this.size / 2,
-            ySize: this.height / 2, zSize: this.size/2 ,
-            center:[-this.width/2 , 0 , 0]});
-    this.add(new xeogl.Entity( {  geometry: post }));
-
-    post = new xeogl.BoxGeometry( { xSize: this.size / 2,
-            ySize: this.height / 2, zSize: this.size/2 ,
-            center:[this.width/2 , 0 , 0]});
-            this.add(post)
-    this.add(new xeogl.Entity( {  geometry: post }));
-
-    var beam = new xeogl.BoxGeometry( { xSize: this.width / 2 + this.size / 2,
-          ySize: this.size / 2, zSize: this.size/2 ,
-          center:[0 ,this.height/2 ,0]});
-    this.add(new xeogl.Entity( {  geometry: beam }))
-  },
-  set_size:function(size){
-
-  },
-});
 eco.Frame = xeogl.Model.extend({
     frame: { truss: {angle: 30, type: 'harja' , on: true} , size: 6 , height: 250,
               spacing: 300, width: 400 , posts: 4 , braces: true },
---
+++ app/assets/javascripts/post_beam.js
@@ -0,0 +1,30 @@
+eco = {}
+
+eco.PostBeam = xeogl.Model.extend({
+  type: "eco.PostBeam",
+  size: 6,
+  height: 200,
+  width: 300,
+  braces: true,
+  _init: function( ){
+    xeogl.Model.prototype._init.apply(this , [{}]); // super()
+    var post = new xeogl.BoxGeometry( { xSize: this.size / 2,
+            ySize: this.height / 2, zSize: this.size/2 ,
+            center:[-this.width/2 , 0 , 0]});
+    this.add(new xeogl.Entity( {  geometry: post }));
+
+    post = new xeogl.BoxGeometry( { xSize: this.size / 2,
+            ySize: this.height / 2, zSize: this.size/2 ,
+            center:[this.width/2 , 0 , 0]});
+            this.add(post)
+    this.add(new xeogl.Entity( {  geometry: post }));
+
+    var beam = new xeogl.BoxGeometry( { xSize: this.width / 2 + this.size / 2,
+          ySize: this.size / 2, zSize: this.size/2 ,
+          center:[0 ,this.height/2 ,0]});
+    this.add(new xeogl.Entity( {  geometry: beam }))
+  },
+  set_size:function(size){
+
+  },
+});
