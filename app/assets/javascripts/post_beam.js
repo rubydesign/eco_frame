257f15f49416da5edb228eb527237d013935e316
Torsten Ruger <torsten@villataika.fi>
2018-02-09 11:13:48 +0800

    refactor methods

--- app/assets/javascripts/post_beam.js
+++ app/assets/javascripts/post_beam.js
@@ -8,22 +8,23 @@ eco.PostBeam = xeogl.Model.extend({
   braces: true,
   _init: function( ){
     xeogl.Model.prototype._init.apply(this , [{}]); // super()
+    this.add_post_at(this.width/2)
+    this.add_post_at(- this.width/2)
+    this.add_beam()
+  },
+  add_post_at: function(at){
     var post = new xeogl.BoxGeometry( { xSize: this.size / 2,
             ySize: this.height / 2, zSize: this.size/2 ,
-            center:[-this.width/2 , 0 , 0]});
+            center:[ at , 0 , 0]});
     this.add(new xeogl.Entity( {  geometry: post }));
-
-    post = new xeogl.BoxGeometry( { xSize: this.size / 2,
-            ySize: this.height / 2, zSize: this.size/2 ,
-            center:[this.width/2 , 0 , 0]});
-            this.add(post)
-    this.add(new xeogl.Entity( {  geometry: post }));
-
+  },
+  add_beam: function(){
     var beam = new xeogl.BoxGeometry( { xSize: this.width / 2 + this.size / 2,
           ySize: this.size / 2, zSize: this.size/2 ,
           center:[0 ,this.height/2 ,0]});
     this.add(new xeogl.Entity( {  geometry: beam }))
   },
+
   set_size:function(size){
 
   },
