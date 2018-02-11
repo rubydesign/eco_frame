3759f520df5657119beded859cebcddbee240e5f
Torsten Ruger <torsten@villataika.fi>
2018-02-11 20:48:03 +0800

    refactor copied rafter code

--- app/assets/javascripts/truss.js
+++ app/assets/javascripts/truss.js
@@ -7,7 +7,8 @@ Truss = xeogl.Model.extend({
   _init: function( ){
     this._super({});
     this.add_king()
-    this.add_rafter()
+    this.add_rafter( 1 )
+    this.add_rafter( -1 )
   },
   rad: function(){
     return this.angle * (Math.PI / 180)
@@ -19,34 +20,22 @@ Truss = xeogl.Model.extend({
             center:[ 0 , truss_height/2, 0]});
     this.add(new xeogl.Entity( {  geometry: post }));
   },
-  add_rafter: function(){
+  add_rafter: function( turn ){
     // rafter_length so the rafters meet at the bottom (?)
     var rafter_length = this.width / ( 2 * Math.cos(this.rad()));
     var rafter = new xeogl.BoxGeometry( { xSize: this.size / 2,
             ySize: rafter_length / 2,  zSize: this.size / 2 ,
-            center:[ -this.size / 2 , rafter_length /2 , 0]});
+            center:[ -turn * this.size / 2 , rafter_length /2 , 0]});
     var raf = new xeogl.Entity( {  geometry: rafter })
     var translate = new xeogl.Translate({
-              xyz: [this.width / 2 + this.size/2, 0, 0]
+              xyz: [turn * (this.width / 2 + this.size/2), 0, 0]
                 });
     raf.transform = new xeogl.Rotate({
           parent: translate,
            xyz: [0, 0, 1], // Rotate angle degrees about y axis
-          angle: 90 - this.angle });
+          angle: turn*(90 - this.angle) });
     this.add( raf );
-    rafter = new xeogl.BoxGeometry( { xSize: this.size / 2,
-            ySize: rafter_length / 2,  zSize: this.size / 2 ,
-            center:[  this.size / 2 , rafter_length /2 , 0]});
-    raf = new xeogl.Entity( {  geometry: rafter })
-    translate = new xeogl.Translate({
-              xyz: [-(this.width / 2 + this.size/2), 0, 0]
-                });
-    raf.transform = new xeogl.Rotate({
-          parent: translate,
-           xyz: [0, 0, 1], // Rotate - angle degrees about y axis
-          angle: this.angle - 90 });
-    this.add( raf );
-  },
+      },
   set_size:function(size){
 
   },
