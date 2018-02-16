1360a70eb296d50c08a5175cd6a079ccd61a7082
Torsten Ruger <torsten@villataika.fi>
2018-02-16 23:01:20 +0800

    fix post and beam
    and braces

--- app/assets/javascripts/frame.js
+++ app/assets/javascripts/frame.js
@@ -1,38 +1,39 @@
 //= require claygl.min
-// require post_beam
+//= require post_beam
 // require truss
 //= require plate
 
 var frame = { truss: {angle: 30, type: 'harja' , on: true} , size: 15 , height: 250,
           spacing: 300, width: 400 , posts: 5 , braces: true }
 
-Frame = function(){
+Frame = function(app){
 
   for(var i=0; i < this.frame.posts; i++){
-    this.add( new Plate() );
-    post = new PostBeam()
+    app._plate = new Plate(app ) ;
     var offset = this.frame.spacing *(this.frame.posts / 2 - i)
-    post.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
-    this.add( post );
-    truss = new Truss()
-    truss.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
-    this.add( truss );
+    app._post = new PostBeam(app , i , offset )
+    //truss = new Truss()
+    //truss.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
+    //this.add( truss );
   }
 };
-
 var app = clay.application.create('#claycanvas', {
       init: function (app) {
-          this._camera = app.createCamera([-400, 200, -1000], [0, 0, 0]);
-          this._mainLight = app.createDirectionalLight([400, -600, 2000]);
-          app.createAmbientLight('white', 0.1);
-          new Plate(app);
-          app.scene.update();
-          app._doRender(app.renderer, app.scene);
-          app.timeline.stop();
+        app._camera = app.createCamera([-400, 200, -1000], [0, 0, 0]);
+        app._mainLight = app.createDirectionalLight([400, 600, 2000]);
+        app.createAmbientLight('white', 0.1);
+        app._frame = Frame(app);
+        app._doRender(app.renderer, app.scene);
+        app._control = new clay.plugin.OrbitControl({target: app._camera,domElement: app.renderer.canvas });
+        app.timeline.pause();
+
       },
+      loop: function (app) {
+          app._control.update(app.frameTime);
+      }
   });
 
-window.update = function(){
-  app.scene.update();
+window.reload = function(){
+  app._frame = Frame(app);
   app._doRender(app.renderer, app.scene);
 }
