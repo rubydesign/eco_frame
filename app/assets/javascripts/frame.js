f00a238868ecbee6aaf2513bdc98d7cb75c0e9c2
Torsten Ruger <torsten@villataika.fi>
2018-02-16 15:00:49 +0800

    basic clay cube

--- app/assets/javascripts/frame.js
+++ app/assets/javascripts/frame.js
@@ -1,36 +1,41 @@
-//= require xeo
-//= require post_beam
-//= require truss
-//= require plate
+//= require claygl.min
+// require post_beam
+// require truss
+// require plate
 
-Frame = xeogl.Model.extend({
-    frame: { truss: {angle: 30, type: 'harja' , on: true} , size: 15 , height: 250,
-              spacing: 300, width: 400 , posts: 5 , braces: true },
+var frame = { truss: {angle: 30, type: 'harja' , on: true} , size: 15 , height: 250,
+          spacing: 300, width: 400 , posts: 5 , braces: true }
 
-    _init: function(){
-      this._super({});
-      for(var i=0; i < this.frame.posts; i++){
-        this.add( new Plate() );
-        post = new PostBeam()
-        var offset = this.frame.spacing *(this.frame.posts / 2 - i)
-        post.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
-        this.add( post );
-        truss = new Truss()
-        truss.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
-        this.add( truss );
-      }
-    },
-    set_posts: function(){
+Frame = function(){
 
-    }
-});
+  for(var i=0; i < this.frame.posts; i++){
+    this.add( new Plate() );
+    post = new PostBeam()
+    var offset = this.frame.spacing *(this.frame.posts / 2 - i)
+    post.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
+    this.add( post );
+    truss = new Truss()
+    truss.transform = new xeogl.Translate({ xyz: [0 , 0, offset] });
+    this.add( truss );
+  }
+};
 
-eco_init = function(){
-  xeogl.scene = new xeogl.Scene({ canvas: "xeocanvas" });
-  xeogl.scene.material.diffuse = [ 0.6, 0.6, 0.7 ] ;
-  xeogl.scene.camera.view.eye = [-400, 200, -1000];
-  new xeogl.CameraControl();
-  new Frame()
+var app = clay.application.create('#claycanvas', {
+      init: function (app) {
+          this._camera = app.createCamera([-400, 200, -1000], [0, 0, 0]);
+          this._cube = app.createCube();
+          this._cube.material.set("color" , "salmon")
+          this._cube.scale.set(300, 300, 300);
+          this._cube.position.set(2,  2, 0);
+          this._mainLight = app.createDirectionalLight([400, -600, 2000]);
+          app.createAmbientLight('white', 0.1);
+          app.scene.update();
+          app._doRender(app.renderer, app.scene);
+          app.timeline.stop();
+      },
+  });
+
+window.update = function(){
+  app.scene.update();
+  app._doRender(app.renderer, app.scene);
 }
-window.reload = eco_init
-eco_init()
