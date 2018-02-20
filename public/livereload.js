ef2aa17e2af1f30ca3eb087fdd5f49601ba84484
Torsten Ruger <torsten@villataika.fi>
2018-02-20 12:32:10 +0800

    switch live reload off
    getting on my nerves and hardly ever works

--- public/livereload.js
+++ public/livereload.js
@@ -1,3 +1,8 @@
+%script
+  window.LiveReloadOptions = { host: 'localhost' , port: '35729', mindelay: 1000000};
+%script{src: "/hotswap.js"}
+%script{src: "/livereload.js"}
+
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function() {
   var Connector, PROTOCOL_6, PROTOCOL_7, Parser, Version, _ref;
