a4944f529b286a979058b5d7877c47a1cd0c0fdf
Torsten Ruger <torsten@villataika.fi>
2018-02-21 20:24:52 +0800

    switch trusses off
    Use NoTruss class

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@ var clay_app = clay.application.create('#claycanvas', {
         app._camera = app.createCamera([-700,100, -1000], [0, 0, 0]);
         app._mainLight = app.createDirectionalLight([400, 600, 2000]);
         app._ambientLight = app.createAmbientLight('white', 0.2);
-        app._frame = new Frame(app , VueFrame._data.frame , VueFrame._data.posts);
+        app._frame = new Frame(app , VueController._data.frame , VueController._data.posts);
         app._doRender(app.renderer, app.scene);
         app._control = new clay.plugin.OrbitControl({
                 target: app._camera,
