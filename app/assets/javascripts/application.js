06b82b434d71c8a7e76bb60d85350d75bcd43167
Torsten Ruger <torsten@villataika.fi>
2018-02-23 17:42:43 +0530

    still trying to update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,25 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application, which will include all the files
 // listed below.
 
+//=require xeo
+//=require vue
+//=require range_slider
+//=require vue_controller
+
+//=require frame
+//= require post_beam
+
+// require plate
+// require range_slider
+// require floor
+// require harja
+// require post_beam
+
 //= require_self
+
+xeogl.scene = new xeogl.Scene({ canvas: "xeocanvas" });
+xeogl.scene.material.diffuse = [ 0.6, 0.6, 0.7 ] ;
+xeogl.scene.camera.view.eye = [-400, 200, -1000];
+new xeogl.CameraControl();
+
+var eco_frame = new Frame();
