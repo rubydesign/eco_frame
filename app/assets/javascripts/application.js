ef7ec40c80fbfe01623ae8920898304084f7bbce
Torsten Ruger <torsten@villataika.fi>
2018-02-27 20:40:58 +0530

    redid floor beams new style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,5 +24,12 @@ xeogl.scene.material.diffuse = [ 0.6, 0.6, 0.7 ] ;
 xeogl.scene.camera.view.eye = [-900, 200, -700];
 xeogl.scene.camera.view.look = [0, 00, 0];
 var camera_control = new xeogl.CameraControl();
-
 var eco_frame = new Frame();
+
+// xeogl.scene.clips.clips = [
+//     new xeogl.Clip({
+//         pos: [0, 0, -300.0],
+//         dir: [1, -1, 1.0],
+//         active: true
+//     })
+// ];
