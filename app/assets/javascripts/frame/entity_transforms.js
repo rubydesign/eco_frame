239b4e3cfc3b141b13386ae86d7a87d4659fb8f3
Torsten Ruger <torsten@villataika.fi>
2018-03-01 17:21:32 +0530

    have to reuse the transformations
    otherwise xeo has fit
    admittedly cleaner, just more work

--- app/assets/javascripts/frame/entity_transforms.js
+++ app/assets/javascripts/frame/entity_transforms.js
@@ -20,8 +20,15 @@ xeogl.Entity.prototype.position = function(x,y,z){
 
 // Bottom side (first param) (point is zero) with given size
 xeogl.Entity.prototype.b_scale = function(side , x,y,z){// bottom left
-  this.transform = new xeogl.Scale( {xyz: [x,y,z] ,
-                  parent: new xeogl.Translate({xyz:[-side*x, y, z]}) } );
+  var scale = this.scene.components["b_scale_" + this.id];
+  if(scale){
+      scale.xyz = [x,y,z];
+      scale.parent.xyz = [-side*x, y, z];
+  }else{
+    this.transform = new xeogl.Scale( {xyz: [x,y,z] , id: "b_scale_" + this.id,
+                    parent: new xeogl.Translate({xyz:[-side*x, y, z]}) } );
+  }
+  return this;
 };
 
 function get_parent(trans){
@@ -32,13 +39,23 @@ function get_parent(trans){
 }
 
 xeogl.Entity.prototype.rotZ = function(deg){
-  var p = get_parent(this.transform)
-  p.parent = new xeogl.Rotate({xyz: [0,0,1] , angle: deg });
+  var rot = this.scene.components["rotz_" + this.id];
+  if(rot){
+    rot.angle = deg;
+  }else{
+    var p = get_parent(this.transform)
+    p.parent = new xeogl.Rotate({xyz: [0,0,1] , angle: deg , id: "rotz_" + this.id});
+  }
   return this;
 };
 
 xeogl.Entity.prototype.trans = function(x,y,z){
-  var p = get_parent(this.transform)
-  p.parent = new xeogl.Translate({xyz: [x,y,z]  });
+  var trans = this.scene.components["trans_" + this.id];
+  if(trans){
+    trans.xyz = [x,y,z];
+  }else{
+    var p = get_parent(this.transform)
+    p.parent = new xeogl.Translate({xyz: [x,y,z] , id: "trans_" + this.id });
+  }
   return this;
 };
