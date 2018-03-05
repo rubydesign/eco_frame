688f081009ae00437bad29b2eab8771200852ae1
Torsten Ruger <torsten@villataika.fi>
2018-03-05 18:11:13 +0530

    remove old style transforms

--- app/assets/javascripts/frame/entity_transforms.js
+++ app/assets/javascripts/frame/entity_transforms.js
@@ -1,23 +1,3 @@
-xeogl.Entity.prototype.rotateZ = function(deg){
-  //console.log("rot" + this.transform.parent.id);
-  this.transform.parent.xyz = [0,0,1];
-  this.transform.parent.angle = deg;
-};
-xeogl.Entity.prototype.rotateX = function(deg){
-  //console.log("rot" + this.transform.parent.id);
-  this.transform.parent.xyz = [1,0,0];
-  this.transform.parent.angle = deg;
-};
-xeogl.Entity.prototype.scale = function(x,y,z){
-  //console.log("scale " + x+ " " +  this.transform.id);
-  this.transform.xyz = [x , y,z];
-};
-xeogl.Entity.prototype.position = function(x,y,z){
-  //console.log("pos " + x+ " " + this.transform.parent.parent.id);
-  this.transform.parent.parent.xyz = [x , y,z];
-};
-
-
 // Bottom side (first param) (point is zero) with given size
 xeogl.Entity.prototype.b_scale = function(side , x,y,z){// bottom left
   var scale = this.scene.components["b_scale_" + this.id];
