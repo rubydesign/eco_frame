8eb28308ea71e065544e6b015f1928f89b593761
Torsten Ruger <torsten@villataika.fi>
2018-03-07 21:23:43 +0530

    slight improvement in materials (outer surfaces)
    leaving debug in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,17 @@ xeogl.scene.material.diffuse = [ 0.6, 0.6, 0.7 ] ;
 xeogl.scene.camera.view.eye = [-830, 50, -780];
 xeogl.scene.camera.view.look = [80, -170, -100];
 var camera_control = new xeogl.CameraControl();
+var materials = {
+  wood:  new xeogl.LambertMaterial({ color: [230/300, 145/300, 60/300]}),
+  floor: new xeogl.LambertMaterial({ color: [0/255, 6/255, 0/255]}),
+  red:   new xeogl.SpecularMaterial({ emissive: [6/255, 0/255, 0/255], glossiness: 0.33,
+               specular: [6/255, 0/255, 0/255], diffuse: [6/255, 0/255, 0/255],}),
+  black: new xeogl.MetallicMaterial({ baseColor: [2/255, 2/255, 2/255],
+            roughness: 0.5 }),
+  inner: new xeogl.LambertMaterial({ color: [100/255, 100/255, 100/255]}),
+  grey:  new xeogl.LambertMaterial({ color: [30/255, 30/255, 30/255]})
+};
+
 var eco_que = new fQueue();
 function once(){
   first = eco_que.shift();
