55768aafb0c7c62615beaf8b3b6b7051af37438b
Torsten Ruger <torsten@villataika.fi>
2018-05-02 11:01:25 +0300

    maths for collar

--- app/assets/javascripts/frame/entity_transforms.js
+++ app/assets/javascripts/frame/entity_transforms.js
@@ -50,3 +50,11 @@ xeogl.Entity.prototype.trans = function(x,y,z){
   }
   return this;
 };
+
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
