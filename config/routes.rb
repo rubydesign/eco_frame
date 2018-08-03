da2d03c6c19256795b3c374bc840c65b548f6967
Torsten Ruger <torsten@villataika.fi>
2018-08-03 16:02:41 +0300

    add route
    must add auto detection later

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  root :to => "high_voltage/pages#show", format: false , locale: :en , id: "index"
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    get "/:id" => "high_voltage/pages#show", :as => :page, :format => false
+    get "/:id" => "high_voltage/pages#show", as: :page, format: false
   end
 
 end
