c1784abbbf111c4bfff60b26342c421afef85233
Torsten Ruger <torsten@villataika.fi>
2018-08-03 16:08:50 +0300

    copy locale files to directories

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module EcoFrame
     config.load_defaults 5.2
     config.i18n.available_locales = [:en, :fi , :se]
     config.i18n.default_locale = :en
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
