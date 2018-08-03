955403e25b3056be290f2e45fe2e00b29e05f56d
Torsten Ruger <torsten@villataika.fi>
2018-08-03 15:10:15 +0300

    set locale, scope routes

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,4 +1,4 @@
+# config/initializers/high_voltage.rb
 HighVoltage.configure do |config|
-  config.home_page = 'index'
-  config.route_drawer = HighVoltage::RouteDrawers::Root
+  config.routes = false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get "/:id" => "high_voltage/pages#show", :as => :page, :format => false
+  end
+
 end
