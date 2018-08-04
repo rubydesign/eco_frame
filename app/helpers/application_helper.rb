2652c88df6b36b245108816873d8d1aa0de7620f
Torsten Ruger <torsten@villataika.fi>
2018-08-04 11:47:51 +0300

    simplify body classses to two
    index and page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     src = asset_path "vue#{prod}.js"
     "<script src='#{src}'></script>".html_safe
   end
+
+  def body_class
+    return "index" if params[:id] == "index"
+    "page"
+  end
+
 end
