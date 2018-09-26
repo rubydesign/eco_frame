ce284391412e4479145f8dbd2c2e8c9bbad5e258
Torsten Ruger <torsten@villataika.fi>
2018-09-26 14:32:27 +0300

    set default locale as per browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 class ApplicationController < ActionController::Base
-  before_action :locale
+  before_action :set_locale
 
-  def locale
-    I18n.locale = params[:locale] || I18n.default_locale
+  def set_locale
+    loc = params[:locale]
+    if(loc)
+      I18n.locale = loc
+    else
+      redirect_to_default
+    end
   end
 
+  def redirect_to_default
+    page = params[:id] || "index"
+    redirect_to "/#{extract_locale}/#{page}"
+  end
+
+  def extract_locale
+    candidates = request.env['HTTP_ACCEPT_LANGUAGE']
+    return I18n.default_locale unless candidates
+    if I18n.available_locales.include?(candidates.scan(/^[a-z]{2}/).first)
+      browser_locale
+    else
+      I18n.default_locale
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root :to => "high_voltage/pages#show", format: false , locale: :en , id: "index"
+  root :to => "high_voltage/pages#show", format: false , id: "index"
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     get "/:id" => "high_voltage/pages#show", as: :page, format: false
