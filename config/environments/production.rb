3fa359bf81ac31e022324a0f992bd15dc69dd0c0
Torsten Ruger <torsten@villataika.fi>
2018-10-07 16:55:00 +0300

    fix secret naming

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,19 +63,19 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
           :address              => "smtp.yandex.com",
           :port                 => 465,
-          :domain               => Rails.application.secrets.domain_name,
-          :user_name            => Rails.application.secrets.admin_email,
-          :password             => Rails.application.secrets.admin_pass,
+          :domain               => Rails.application.credentials.domain_name,
+          :user_name            => Rails.application.credentials.admin_email,
+          :password             => Rails.application.credentials.admin_pass,
           :ssl                  => true,
           :tls                  => true,
           :authentication       => :login,
           :enable_starttls_auto => true
     }
-    config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+    config.action_mailer.default_url_options = { :host => Rails.application.credentials.domain_name }
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_options = {  from: Rails.application.secrets.admin_email ,
-                                              bcc:  Rails.application.secrets.admin_email }
+    config.action_mailer.default_options = {  from: Rails.application.credentials.admin_email ,
+                                              bcc:  Rails.application.credentials.admin_email }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
