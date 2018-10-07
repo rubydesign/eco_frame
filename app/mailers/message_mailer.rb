4a0d2a46180fb5fee1642aad3bf0c64a7d4a1d20
Torsten Ruger <torsten@villataika.fi>
2018-10-07 16:36:00 +0300

    create actual message template

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,7 @@
 class MessageMailer < ApplicationMailer
 
   def submit( message )
+    @message = message
     email = message.email
     mail(to: email, reply_to: email , subject: "Your message")
   end
