50a4ef433bbd785ad27b8721e7d7e6e8f4673831
Torsten Ruger <torsten@villataika.fi>
2018-10-07 16:29:21 +0300

    fix logo and message send

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,9 +1,8 @@
 class MessageMailer < ApplicationMailer
 
   def submit( message )
-    email = @message.email
-    mail(to: email, reply_to: email ,
-      subject: "Your message")
+    email = message.email
+    mail(to: email, reply_to: email , subject: "Your message")
   end
 
 end
